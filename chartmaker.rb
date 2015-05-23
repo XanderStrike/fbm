6eb56c713fd126db9e7d95c89d630a8f332a9305
Alexander Standke <xanderstrike@gmail.com>
2015-05-22 22:07:21 -0700

    refactor and add message length

--- chartmaker.rb
+++ chartmaker.rb
@@ -1,30 +1,58 @@
+# fbm - chartmaker
+#  alex standke
+#  may 2015
+#
+# generates charts based on databased facebook messages
+
 require 'mysql2'
 require 'gruff'
 
-client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'fb_messages')
 
-THEME = Gruff::Themes::PASTEL
+# -- settings --
+
+THEME = Gruff::Themes::RAILS_KEYNOTE
 MYNAME = 'Alexander Standke'
 
-def init_gruff klass, size=1000
-  g = klass.new(size)
-  g.theme = THEME
-  g
+
+# -- mysql --
+
+client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'fb_messages')
+
+
+# -- simple grunt initializer service --
+
+class G
+  class << self
+    def init_line title
+      init(Gruff::Line, title)
+    end
+
+    def init_dot title
+      init(Gruff::Dot, title)
+    end
+
+    def init_area title
+      init(Gruff::Area, title, hide_legend: true)
+    end
+
+    def init_bar title
+      init(Gruff::Bar, title, hide_legend: true)
+    end
+
+    private
+
+    def init klass, title, size: 1000, hide_legend: false
+      g = klass.new(size)
+      g.title = title
+      g.theme = THEME
+      g.hide_legend = hide_legend
+      g
+    end
+  end
 end
 
-# def init_gruff_line title, size=1000
-#   g = Gruff::Line.new(size)
-#   g.theme = THEME
-#   g.title = title
-#   g
-# end
 
-def init_gruff_bar title, size=1000
-  g = init_gruff(Gruff::Bar)
-  g.title = title
-  g.hide_legend = true
-  g
-end
+# -- data massaging --
 
 def data_from_num g, results
   formatted_data = results.map { |r| r['num'] }
@@ -34,48 +62,57 @@ def data_from_num g, results
   g
 end
 
+def number_labels num, multiplier=1
+  {}.tap do |labels|
+    num.times do |x|
+      labels[(x * multiplier)] = (x * multiplier).to_s
+    end
+  end
+end
+
+
+# -- charts --
+
 def by_hour client
   results = client.query('SELECT HOUR(time) as hour, COUNT(1) as num FROM messages GROUP BY HOUR(time)').to_a
 
-  g = init_gruff_bar 'Messages by Hour of Day'
-  g.labels = {}.tap do |labels|
-    24.times do |x|
-      labels[x] = x.to_s
-    end
-  end
+  g = G.init_area 'Messages by Hour of Day'
+  g.labels = number_labels(24)
 
   g = data_from_num(g, results)
 
   g.write('images/by_hour.png')
 end
 
+by_hour client
+
+
 def by_day_of_week client
   results = client.query('SELECT DAYOFWEEK(time) as day, COUNT(1) as num FROM messages GROUP BY DAYOFWEEK(time)').to_a
 
-  g = init_gruff_bar 'Messages by Day of Week'
+  g = G.init_bar 'Messages by Day of Week'
 
-  g.labels = {
-    0 => 'Sun',
-    1 => 'Mon',
-    2 => 'Tue',
-    3 => 'Wed',
-    4 => 'Thu',
-    5 => 'Fri',
-    6 => 'Sat',
-  }
+  g.labels = {}.tap do |h|
+    %w(Sun Mon Tue Wed Thu Fri Sat).each_with_index do |day, i|
+      h[i] = day
+    end
+  end
 
   g = data_from_num(g, results)
 
   g.write('images/by_day_of_week.png')
 end
 
+by_day_of_week client
+
+
 def top_conversations client
   results = client.query('select thread, count(1) as num from messages group by thread order by num desc limit 20').to_a
 
-  g = init_gruff_bar 'Top 20 Conversations'
+  g = G.init_bar 'Top 20 Conversations'
+  g.x_axis_label = 'Initial of Users(s)'
   g.label_max_size = 4
   g.marker_font_size = 10
-  g.x_axis_label = 'Initial of Users(s)'
 
   g.labels = {}.tap do |labels|
     20.times do |x|
@@ -89,7 +126,22 @@ def top_conversations client
   g.write('images/top_conversations.png')
 end
 
-by_hour client
-by_day_of_week client
 top_conversations client
 
+
+def message_length client
+  results = client.query('select length(message) as length, count(1) as num from messages group by length limit 150')
+  average = client.query('select avg(length(message)) as average from messages').to_a[0]["average"].to_f
+  max = client.query('select max(length(message)) as average from messages').to_a[0]["average"].to_i
+
+  g = G.init_area 'Message Length'
+  g.x_axis_label = "Max: #{ max }  Average: #{ average }"
+
+  g.labels = number_labels(15, 10)
+
+  g = data_from_num(g, results)
+
+  g.write('images/message_length.png')
+end
+
+message_length client
--- db_populator.rb
+++ db_populator.rb
@@ -1,8 +1,18 @@
+# fbm - db_populator
+#  alex standke
+#  may 2015
+#
+# scrapes a facebook message backup into a mysql table
+#
+# it ain't pretty but it works
+
 require 'mysql2'
 require 'nokogiri'
 require 'time'
 
+
 puts 'Creating DB'
+
 client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'fb_messages')
 client.query('DROP TABLE messages') rescue nil
 client.query(<<-SQL
@@ -15,21 +25,28 @@ client.query(<<-SQL
   SQL
   )
 
+
 puts 'Parsing HTML'
+
 f = File.open('messages.htm')
 doc = Nokogiri::HTML(f)
 f.close
 
+
 puts 'Storing Messages in DB'
 
 index = 0
-
 threads = doc.css('.thread')
 threads.each do |thread|
   participants = thread.children.first.text
+
+
   puts "Processing thread: #{ participants[0..55] }..."
+
   messages = thread.css(".message")
   messages.each do |message|
+
+
     print "Storing message #{index}\r"
 
     insert_query = <<-SQL
